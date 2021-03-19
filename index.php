e05b8abb5f8d52bc91e04782b90729ca5f3730f2
gimhanrd9 <79823351+gimhanrd9@users.noreply.github.com>
2021-03-19 13:39:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['loggedin'])){
         <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
-                    <h2>Car Rental<em> Website</em></h2>
+                    <h2>CarRodio</h2>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
