7e392f3508ffc099eb3b2f62802a673072be0327
gimhanrd9 <79823351+gimhanrd9@users.noreply.github.com>
2021-04-15 21:19:03 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,11 +1,33 @@
-function toggle() {
-    document.getElementsByClassName("myDropdown").classList.toggle("show");
+document.getElementById("ham_icon").addEventListener("click", menuToggle);
+document.getElementById("sub_menu").addEventListener("click", toggleNav);
+document.getElementById("sub_menu1").addEventListener("click", toggleNavLog);
+
+function toggleNav() {
+    var subItems = document.getElementById("myDropdown");
+
+    if (subItems.className === "dropdown-menu") {
+        subItems.className += "submenu";
+        subItems.classList.toggle("show");
+
+    } else {
+        subItems.className = "dropdown-menu";
+    }
 }
 
+function toggleNavLog() {
+    var subItems1 = document.getElementById("myDropdown1");
+
+    if (subItems1.className === "dropdown-menu") {
+        subItems1.className += "submenu";
+        subItems1.classList.toggle("show");
+
+    } else {
+        subItems1.className = "dropdown-menu";
+    }
+}
 
 var slideIndex = 1;
 showSlides(slideIndex);
-
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -26,5 +48,16 @@ function showSlides(n) {
     }
 
     slides[slideIndex - 1].style.display = "block";
+
+}
+
+function menuToggle() {
+    var menu = document.getElementById("toggle_nav");
+    if (menu.style.display == "none") {
+
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none"
+    }
 
 }
