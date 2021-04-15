3debbcfbe04e80bf1f34ba242fbce0b656c598d0
gimhanrd9 <79823351+gimhanrd9@users.noreply.github.com>
2021-04-15 21:20:35 +0530

    Update sellerDashboard.php

--- sellerDashboard.php
+++ sellerDashboard.php
@@ -15,8 +15,9 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']==false) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/seller.css">
-    <link rel="stylesheet" href="css/fontawesome.css">
+
 
 </head>
 
